49a5fec75033809dadc5e2fe70f33902d9c36145
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-02-10 15:54:10 +0530

    add LatitudeError & LongitudeError in exceptions
    - LatitudeError: Can be raised for any invalid latitude(s).
    - LongitudeError: Can be raised for any invalid longitude(s).
    
    Based on these rules:
    - Order: Strict [North, West, South, East] sequence (Clockwise from top).
    - Latitude Bounds: All values must be within [-90, 90].

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,9 @@
 class ValidationError(Exception):
     """Raised when request parameters are out of range or malformed"""
     pass
+
+class LatitudeError(ValidationError):
+    """Raised when rules of defining latitudes are not followed."""
+
+class LongitudeError(ValidationError):
+    """Raised when rules of defining longitudes are not followed."""
