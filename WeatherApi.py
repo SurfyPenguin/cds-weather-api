beb84176d020a9e1682b6b5af1ec0b467d380b40
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-02-10 15:08:44 +0530

    Re: add docstrings for type aliases

--- WeatherApi.py
+++ WeatherApi.py
@@ -2,7 +2,28 @@ import cdsapi
 from typing import Self, Callable
 
 type ParameterList = list[str]
+"""
+Representation for list which contains string parameters.
+
+Example:
+
+    product_type = ['reanalysis']
+    day = ["01", "02", "03", ..., "12"]
+    month = ["01", "02", ..., "30"]
+"""
+
 type BoundingBox = list[int | float]
+"""
+Represents a geographical area as a list of four coordinates.
+
+Format:
+
+    [North, West, South, East]
+
+Example:
+
+    [90, -120, -90, 180]
+"""
 
 class WeatherApi:
     """
