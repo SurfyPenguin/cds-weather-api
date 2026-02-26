f142ba7996fbe8567c114401cf1d743ab9e0f53f
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-02-26 19:28:30 +0530

    chore: bump version v2.1.0
    This bump extends the functionality and context of cdsapi.Client class
    by allowing configs to be passed in RequestBuilder method.
    
    Configs can be used to provide API KEY & API URL from various ways
    such as from environment variables.
    
    RequestBuilder now has its own Exception which is raised through
    ClientConfig class; Raised when API KEY & API URL are not mentioned:
    - in config or
    - in .cdsapirc file
    
    API credentials explicitly passed to create a config/client instance
    override the API credentials provided in .cdsapirc file,
    Provided the instance is passed to RequestBuidler.

--- src/CdsApi/__init__.py
+++ src/CdsApi/__init__.py
@@ -3,7 +3,7 @@ from .exceptions import *
 from .helpers import CDSFormatter
 from .weather_api import RequestBuilder
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __author__ = "SurfyPenguin"
 
 __all__ = [
