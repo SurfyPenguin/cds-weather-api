d7b6806bf780b65465adf8adcc25119c74b5c34b
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-02-28 04:01:54 +0530

    re: move types to types.py for SoC

--- src/CdsApi/weather_api.py
+++ src/CdsApi/weather_api.py
@@ -11,30 +11,8 @@ from .helpers import (
     EN_DASH,
 )
 import os
-
-ParameterList = list[str]
-"""
-Representation for list which contains string parameters.
-
-Example:
-
-    product_type = ['reanalysis']
-    day = ["01", "02", "03", ..., "12"]
-    month = ["01", "02", ..., "30"]
-"""
-
-BoundingBox = tuple[float, float, float, float]
-"""
-Represents a geographical area as a list of four coordinates.
-
-Format:
-
-    (North, West, South, East)
-
-Example:
-
-    (90, -120, -90, 180)
-"""
+from .types import ParameterList, BoundingBox
+from typing import Union
 
 class WeatherApi:
     """
