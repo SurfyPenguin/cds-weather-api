3ae29cc2f0ef447969881371b7c6c6d95ca05f60
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-03-05 18:17:54 +0530

    feat: add exception tests for validators.Validators
    - added 8 tests for exceptions

--- tests/unit/test_validators.py
+++ tests/unit/test_validators.py
@@ -1,7 +1,21 @@
+from CdsApi.exceptions import ValidationError
 from CdsApi.validators import Validators
 import pytest
 
 class TestValidators:
+
+    INVALID_INPUTS = [
+        [(), int], # empty tuple
+        [[], int], # empty list
+
+        [["10m_u_component_of_wind", 2, 3, 4], int],
+        [["10m_u_component_of_wind", 2, 3, 4], float],
+        [["10m_u_component_of_wind", 2, 3, 4], str],
+        [["10m_u_component_of_wind", 2, 3, 4.0], (int, str)],
+        [[1, 2, 3, 4], float],
+        [[0.22, 5.89, 3.45, 1.12], int],
+    ]
+
     @pytest.mark.parametrize("data, types", [
         (["10m_u_component_of_wind", "10m_v_component_of_wind"], str), # check string
         ([2021, 2022, 2023], int), # check int
@@ -11,3 +25,9 @@ class TestValidators:
     def test_list_of_type(self, data, types):
         """Test that valid lists and tuples pass without any exception."""
         Validators.list_of_type(data, types=types)
+
+    @pytest.mark.parametrize("data, types", INVALID_INPUTS)
+    def test_list_of_type_invalid(self, data, types):
+        """Test that exceptions are raised for a set of invalid values."""
+        with pytest.raises(ValidationError):
+            Validators.list_of_type(data, types)
