ed889e0acfb5758e6283cae9b54beff2e548a4f9
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-03-15 01:41:11 +0530

    chore: add build system & update metadata in pyproject
    It makes project ready for publishing to PyPI in future.

--- src/CdsApi/__init__.py
+++ src/CdsApi/__init__.py
@@ -2,6 +2,7 @@ from .client_config import ClientConfig
 from .exceptions import *
 from .helpers import CDSFormatter
 from .request_builder import RequestBuilder
+from .types import *
 
 __version__ = "2.1.3"
 __author__ = "SurfyPenguin"
@@ -11,6 +12,8 @@ __all__ = [
     "ClientConfig",
     "ClientError",
     "RequestBuilder",
+    "BuildError",
+    "ClientError",
     "LatitudeError",
     "LongitudeError",
     "ValidationError",
